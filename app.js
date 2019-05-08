ae0668f2d8b065ffe2a9f1c138135c0701b2cce6
Gaurav Dadlaney <gaurav18n@gmail.com>
2019-05-08 18:47:45 +0530

    Initial DB setup

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const mysql = require("mysql2/promise");
 
 const PORT = 5000;          // get from env
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'twitterAPI',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});  
 
 // set up express app instance
 app = express();
@@ -9,6 +18,15 @@ app = express();
 app.get("/hello", (req, res) => {
     res.send("Hello World");
 });
+
+// for debugging
+// Lists all registered users
+app.get("/users", getUsers);
+
+async function getUsers(req, res) {
+    const result = await pool.query("SELECT * FROM users;");
+    res.send(result[0]);
+}
 
 // app.post("/register", handleRegister);
 // app.post("/login" handleLogin);
---
+++ twitterAPI.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 08, 2019 at 06:47 PM
+-- Server version: 10.0.38-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.0.33-0ubuntu0.16.04.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `twitterAPI`
+--
+CREATE DATABASE IF NOT EXISTS `twitterAPI` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `twitterAPI`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES
+(1, 'gd', 'gd');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
